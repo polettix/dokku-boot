7e04d0a605c66472aad9637ab5299f0be42466c5
Flavio Poletti <flavio@polettix.it>
2016-10-06 00:53:36 +0200

    Accept 80/443 from docker0 too

--- 00.setup-iptables.sh
+++ 00.setup-iptables.sh
@@ -14,10 +14,12 @@ if [ -n "$REGEN_RULES" -o ! -e "$RULES" ] ; then
 :OUTPUT DROP [58:4872]
 -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT 
 -A INPUT -i lo -j ACCEPT 
--A INPUT -i eth0 -p icmp --icmp-type echo-request -m limit --limit 2/second -j ACCEPT
--A INPUT -i eth0 -p tcp -m tcp --dport    22 -j ACCEPT 
--A INPUT -i eth0 -p tcp -m tcp --dport    80 -j ACCEPT 
--A INPUT -i eth0 -p tcp -m tcp --dport   443 -j ACCEPT 
+-A INPUT -i eth0    -p icmp --icmp-type echo-request -m limit --limit 2/second -j ACCEPT
+-A INPUT -i eth0    -p tcp -m tcp --dport    22 -j ACCEPT 
+-A INPUT -i eth0    -p tcp -m tcp --dport    80 -j ACCEPT 
+-A INPUT -i eth0    -p tcp -m tcp --dport   443 -j ACCEPT 
+-A INPUT -i docker0 -p tcp -m tcp --dport    80 -j ACCEPT 
+-A INPUT -i docker0 -p tcp -m tcp --dport   443 -j ACCEPT 
 -A OUTPUT -s 127.0.0.1 -j ACCEPT
 END
    
