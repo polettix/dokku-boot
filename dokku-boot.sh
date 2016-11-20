14ce956bc5e2c4fd70fe1d514c5edc44329960d9
Flavio Poletti <flavio@polettix.it>
2016-11-20 01:16:02 +0100

    Load env.sh remotely if available

--- dokku-boot.sh
+++ dokku-boot.sh
@@ -6,6 +6,8 @@ ME="$(readlink -f "$0")"
 MD="$(dirname "$ME")"
 SD="$MD/$(basename "$ME" .sh).d"
 
+[ -r "$MD/env.sh" ] && . "$MD/env.sh"
+
 find "$SD" -type f -executable \
    | LC_ALL=C sort             \
    | while read S ; do
