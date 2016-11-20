5f138045693b0918a4348ea8b7f507bdd9844227
Flavio Poletti <flavio@polettix.it>
2016-11-20 12:34:25 +0100

    Apply some refactoring

--- dokku-boomote.sh
+++ dokku-boomote.sh
@@ -1,73 +1,97 @@
 #!/bin/bash
-set -eo pipefail
 
-ME="$(readlink -f "$0")"
-MD="$(dirname "$ME")"
-PROGRAM="dokku-boot.pl"
-export DOMAIN="$1"
-DOKKU_IP="$2"
+main() {
+   set -eo pipefail
 
-if [ -e 'env.sh' ] ; then
-   . 'env.sh'
-fi
+   set_environment "$@"
 
-if [ -z "$DOMAIN" ] ; then
-   echo >&2 "no domain provided"
-   exit 1
-fi
-export DOKKU_HOSTNAME="$DOMAIN"
+   if [ -z "$DOKKU_IP" ] ; then
+      DOKKU_IP="$(instantiate_vm)"
+   fi
 
-if [ -z "$DOKKU_IP" ] ; then
+   CREDENTIALS="root@$DOKKU_IP"
+   wait_vm_up
+
+   set_DNS
+
+   remote_operations
+
+   echo "*.$DOKKU_HOSTNAME => $DOKKU_IP"
+}
+
+set_environment() {
+   ME="$(readlink -f "$0")"
+   MD="$(dirname "$ME")"
+
+   DOKKU_HOSTNAME="${1:-"$DOKKU_HOSTNAME"}"
+   : ${DOKKU_VHOST_ENABLE:='true'}
+   : ${DOKKU_WEB_CONFIG:='false'}
+   : ${DOKKU_KEY_FILE:='/root/.ssh/authorized_keys'}
+   DOKKU_IP="$2"
+
+   if [ -e 'env.sh' ] ; then
+      . 'env.sh'
+   fi
+
+   if [ -z "$DOKKU_HOSTNAME" ] ; then
+      echo >&2 "no domain, set DOKKU_HOSTNAME or pass a domain name"
+      return 1
+   fi
+
+   return 0
+}
+
+instantiate_vm() {
    echo >&2 "instantiating VM via $VM_PROVIDER"
-   DOKKU_IP="$("$MD/new-vm.d/$VM_PROVIDER.sh")"
-fi
+   local RVAL="$(DOMAIN="$DOKKU_HOSTNAME" "$MD/new-vm.d/$VM_PROVIDER.sh")"
+   if [ -n "$RVAL" ] ; then
+      echo "$RVAL"
+      return 0
+   fi
 
-if [ -z "$DOKKU_IP" ] ; then
    echo >&2 "cannot get an IP to connect to"
-   exit 1
-fi
+   return 1
+}
 
-CREDENTIALS="root@$DOKKU_IP"
-AVAILABLE=''
-for SLEEP_TIME in 0 1 2 3 4 5 ; do
-   sleep "$SLEEP_TIME"
-   ssh >/dev/null 2>&1 \
-      -o ConnectTimeout="$((SLEEP_TIME + 1))" \
-      -o UserKnownHostsFile=/dev/null \
-      -o StrictHostKeyChecking=no \
-      "$CREDENTIALS" ls / && AVAILABLE=yes
-   [ -n "$AVAILABLE" ] && break
-done
+wait_vm_up() {
+   for SLEEP_TIME in 0 1 2 3 4 5 ; do
+      sleep "$SLEEP_TIME"
+      ssh >/dev/null 2>&1 \
+            -o ConnectTimeout="$((SLEEP_TIME + 1))" \
+            -o UserKnownHostsFile=/dev/null \
+            -o StrictHostKeyChecking=no \
+            "$CREDENTIALS" ls / \
+         && return 0
+   done
 
-if [ -z "$AVAILABLE" ] ; then
-   echo >&2 "cannot SSH to $DOKKU_IP"
-   exit 1
-fi
+   echo "cannot SSH to $DOKKU_IP"
+   return 1
+}
 
-echo >&2 "setting *.$DOMAIN to $DOKKU_IP via $DNS_PROVIDER"
-if ! "$MD/dns.d/$DNS_PROVIDER.sh" set-wildcard-address "$DOMAIN" "$DOKKU_IP" ; then
-   echo >&2 "cannot set DNS"
-   exit 1
-fi
+set_DNS() {
+   if [ -z "$DNS_PROVIDER" ] ; then
+      echo >&2 "no DNS_PROVIDER, skipping DNS setting"
+      return 0
+   fi
 
-(
-   echo "export DOKKU_VHOST_ENABLE='$DOKKU_VHOST_ENABLE'"
-   echo "export DOKKU_WEB_CONFIG='$DOKKU_WEB_CONFIG'"
-   echo "export DOKKU_KEY_FILE='$DOKKU_KEY_FILE'"
-   echo "export DOKKU_HOSTNAME='$DOKKU_HOSTNAME'"
-) > 'remote-env.sh'
+   echo "setting *.$DOKKU_HOSTNAME to $DOKKU_IP via $DNS_PROVIDER"
+   "$MD/dns.d/$DNS_PROVIDER.sh" \
+         set-wildcard-address "$DOKKU_HOSTNAME" "$DOKKU_IP" \
+      && return 0
+   echo "cannot set DNS"
+   return 1
+}
 
-scp \
-   -o UserKnownHostsFile=/dev/null \
-   -o StrictHostKeyChecking=no \
-   "remote-env.sh" "$CREDENTIALS:/root/env.sh"
-scp \
-   -o UserKnownHostsFile=/dev/null \
-   -o StrictHostKeyChecking=no \
-   "$MD/$PROGRAM" "$CREDENTIALS:/root/$PROGRAM"
-ssh \
-   -o UserKnownHostsFile=/dev/null \
-   -o StrictHostKeyChecking=no \
-   "$CREDENTIALS" perl "/root/$PROGRAM"
+remote_operations() {
+   local NH='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+   (
+      echo "export DOKKU_VHOST_ENABLE='$DOKKU_VHOST_ENABLE'"
+      echo "export DOKKU_WEB_CONFIG='$DOKKU_WEB_CONFIG'"
+      echo "export DOKKU_KEY_FILE='$DOKKU_KEY_FILE'"
+      echo "export DOKKU_HOSTNAME='$DOKKU_HOSTNAME'"
+   ) | ssh $NH "$CREDENTIALS" tee /root/env.sh
+   scp $NH "$MD/dokku-boot.pl" "$CREDENTIALS:/root/dokku-boot.pl"
+   ssh $NH "$CREDENTIALS" perl "/root/dokku-boot.pl"
+}
 
-echo >&2 "*.$DOMAIN => $DOKKU_IP"
+main "$@" >&2
