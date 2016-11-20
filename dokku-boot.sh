1bc69deacaaabe75783c728849c4c84ebd8fbc97
Flavio Poletti <flavio@polettix.it>
2016-11-20 02:12:02 +0100

    Remove printing of OLD_PWD

--- dokku-boot.sh
+++ dokku-boot.sh
@@ -6,7 +6,6 @@ ME="$(readlink -f "$0")"
 MD="$(dirname "$ME")"
 SD="$MD/$(basename "$ME" .sh).d"
 
-echo >&2 -e "\nOLD_PWD[$OLD_PWD]\n"
 [ -r "$OLD_PWD/env.sh" ] && . "$OLD_PWD/env.sh"
 
 find "$SD" -type f -executable \
