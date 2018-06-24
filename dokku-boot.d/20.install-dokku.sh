62b448acc0b05d94bf43c1908a62ba0b9923c5a1
Flavio Poletti <flavio@polettix.it>
2018-06-24 23:35:33 +0200

    Ensure iptables configs are regenerated

--- dokku-boot.d/20.install-dokku.sh
+++ dokku-boot.d/20.install-dokku.sh
@@ -12,5 +12,3 @@ fi
 export DOKKU_TAG
 wget https://raw.githubusercontent.com/dokku/dokku/"$DOKKU_TAG"/bootstrap.sh
 bash bootstrap.sh
-
-"$(dirname "$0")/00.setup-iptables.sh" regenerate
---
+++ dokku-boot.d/21.update-iptables.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+"$(dirname "$0")/00.setup-iptables.sh" regenerate
