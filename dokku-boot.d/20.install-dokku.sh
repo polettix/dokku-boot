2555662c54c0e56ac7f7a89311d7de939fcee4ea
Flavio Poletti <flavio@polettix.it>
2018-06-24 22:40:20 +0200

    Get latest DOKKU_TAG from website

--- dokku-boot.d/20.install-dokku.sh
+++ dokku-boot.d/20.install-dokku.sh
@@ -2,9 +2,14 @@
 
 id dokku >/dev/null 2>&1 && exit 0
 
-: ${DOKKU_TAG:='v0.7.2'}
-export DOKKU_TAG
+if [ -z "$DOKKU_TAG" ] ; then
+   DOKKU_TAG="$(
+      curl https://raw.githubusercontent.com/dokku/dokku/master/README.md \
+      | sed -n 's#wget https://.*/dokku/\(v.*\)/bootstrap.sh#\1#p'
+   )"
+fi
 
+export DOKKU_TAG
 wget https://raw.githubusercontent.com/dokku/dokku/"$DOKKU_TAG"/bootstrap.sh
 bash bootstrap.sh
 
